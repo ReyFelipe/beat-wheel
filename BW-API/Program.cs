9db822430e82aa2d9fc2b81c3e7531b16764aa2e
HackaPhil <philip.gyesi-appiah@durham.ac.uk>
2026-01-01 16:13:02 +0000

    add db migration code

--- BW-API/Program.cs
+++ BW-API/Program.cs
@@ -102,6 +102,13 @@ builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSet
 
 var app = builder.Build();
 
+// Database migration
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {    
